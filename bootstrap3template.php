65209d9258dfbb3f42902d8a6fd4b1e203717a75
belunatic <abelluvyou@yahoo.com>
2017-09-25 16:13:02 -0700

    Bootstrap 3 template and CSS

---
+++ bootstrap3template.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?php echo $desc; ?>">
+    <meta name="author" content="Medical Revitalization">
+    <meta name="keywords" content="<?php echo $keyword; ?>">
+    <title>
+        <?php echo $title; ?>
+    </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="bootstrap3CSS.css" />
+</head>
+
+<body>
+    <!-- main wrapper -->
+    <div id="mainWrapper">
+        <!-- content -->
+        <section class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <!-- header -->
+                    <header>
+                        <img class="img-responsive" src="pngLogo/frontLogo300.png" alt="Abel Brian web developer logo" />
+                    </header>
+                    <!-- /header -->
+                    <!-- nav -->
+                    <nav>
+                        <ul class="navbar navbar-default" id="homeNav">
+                            <li role="presentation" class="active"><a href="#">Home</a></li>
+                            <li role="presentation"><a href="#">About Us</a></li>
+                            <li role="presentation"><a href="#">Works</a></li>
+                        </ul>
+                    </nav>
+                    <!-- /nav -->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                    </p>
+                </div>
+            </div>
+        </section>
+        <!-- /container -->
+        <!--footer Wrapper -->
+        <div id="footerWrapper">
+            <footer class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <p>&copy;2017 abel brian web developers</p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <!-- /mainWrapper -->
+</body>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous">
+</script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</html>
